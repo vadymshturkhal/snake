908e3d88c676a79518f05ed18ed641806c419af6
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-03-03 15:39:49 +0200

    Splitted function calculate_distance_and_angle.

--- game_utils.py
+++ game_utils.py
@@ -20,12 +20,17 @@ BLUE1 = (0, 0, 255)
 BLUE2 = (0, 100, 255)
 BLACK = (0,0,0)
 
-def calculate_distance_and_angle(snake_head, food_head):
+def calculate_distance(snake_head, food_head):
     dx = food_head.x - snake_head.x
     dy = food_head.y - snake_head.y
     distance = math.sqrt(dx**2 + dy**2)
     angle = math.atan2(dy, dx)
     return distance, angle
+
+def calculate(snake_head, food_head):
+    dx = food_head.x - snake_head.x
+    dy = food_head.y - snake_head.y
+    return math.atan2(dy, dx)
 
 def normalize_distance(distance, max_distance):
     return distance / max_distance
