60462028d8e9fd22b2f73cded30a575a5baa74a2
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-03-13 21:46:33 +0200

    Removed unused method from snake class.

--- snake.py
+++ snake.py
@@ -20,69 +20,6 @@ class Snake:
         if self.direction is None:
             self.direction = random.choice([Direction.RIGHT, Direction.LEFT, Direction.UP, Direction.DOWN])
 
-    def move(self, action, is_human=False) -> bool:
-        self.prev_direction = self.direction
-
-        if is_human:
-            self.direction = action
-        else:
-            # [straight, right, left]
-            clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
-
-            # Current direction index
-            idx = clock_wise.index(self.direction)
-
-            if np.array_equal(action, [1, 0, 0]):
-                new_dir = clock_wise[idx] # no change
-            elif np.array_equal(action, [0, 1, 0]):
-                next_idx = (idx + 1) % DIRECTIONS_QUANTITY
-                new_dir = clock_wise[next_idx] # right turn r -> d -> l -> u
-            elif np.array_equal(action, [0, 0, 1]): # [0, 0, 1]
-                next_idx = (idx - 1) % DIRECTIONS_QUANTITY
-                new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
-            else:
-                raise Exception('Unknown direction', )
-
-            self.direction = new_dir
-
-        x = self.head.x
-        y = self.head.y
-
-        is_crashed = False
-
-        if self.direction == Direction.RIGHT:
-            x_new = x + BLOCK_SIZE
-            if x_new < SCREEN_W:
-                x = x_new
-            else:
-                is_crashed = True
-
-        elif self.direction == Direction.LEFT:
-            x_new = x - BLOCK_SIZE
-            if x_new >= 0:
-                x = x_new
-            else:
-                is_crashed = True
-
-        elif self.direction == Direction.DOWN:
-            y_new = y + BLOCK_SIZE
-            if y_new < SCREEN_H:
-                y = y_new
-            else:
-                is_crashed = True
-
-        elif self.direction == Direction.UP:
-            y_new = y - BLOCK_SIZE
-            if y_new >= 0:
-                y = y_new
-            else:
-                is_crashed = True
-
-        self.head = Point(x, y)
-
-        self._rotate_sprite()
-        return is_crashed
-
     def rotate_snake(self, desired_direction: str):
         if desired_direction == 'left':
             # Rotate left
