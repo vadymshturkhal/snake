df46bf34ee3852da7d8892557e4a74069881e9a2
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-03-17 21:34:28 +0200

    Added batch normalization to model.

--- model.py
+++ model.py
@@ -10,8 +10,10 @@ class Linear_QNet(nn.Module):
     def __init__(self, input_layer, hidden1, hidden2, output_layer):
         super().__init__()
         self.linear1 = nn.Linear(input_layer, hidden1)
+        self.bn1 = nn.BatchNorm1d(hidden1)
         self.dropout1 = nn.Dropout(DROPOUT_RATE)
         self.linear2 = nn.Linear(hidden1, hidden2)
+        self.bn2 = nn.BatchNorm1d(hidden2)
         self.dropout2 = nn.Dropout(DROPOUT_RATE)
         self.linear3 = nn.Linear(hidden2, output_layer)
 
