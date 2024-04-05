14e57c50ba77cf0787dbbe9961587cf2c4e178c5
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-04-05 13:04:58 +0300

    Created class Double Q-learning and model for it.

---
+++ agents/double_qlearning.py
@@ -0,0 +1,120 @@
+import torch
+import numpy as np
+from model import Linear_QNet, DoubleQTrainer
+
+from game_settings import EPSILON_SHIFT, LR, SNAKE_ACTION_LENGTH, BLOCK_SIZE
+from game_settings import SNAKE_INPUT_LAYER_SIZE, SNAKE_HIDDEN_LAYER_SIZE1, SNAKE_HIDDEN_LAYER_SIZE2, SNAKE_OUTPUT_LAYER_SIZE
+from game_settings import SNAKE_GAMMA, SNAKE_MIN_EPSILON, SNAKE_START_EPSILON
+
+class DoubleQLearning:
+    def __init__(self, is_load_weights=False, weights_filename=None, epochs=100, is_load_n_games=True):
+        self.epsilon = 1
+        self.epochs = epochs
+
+        self.gamma = SNAKE_GAMMA
+
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.model1 = Linear_QNet(SNAKE_INPUT_LAYER_SIZE, SNAKE_HIDDEN_LAYER_SIZE1, SNAKE_HIDDEN_LAYER_SIZE2, SNAKE_OUTPUT_LAYER_SIZE)
+        self.model2 = Linear_QNet(SNAKE_INPUT_LAYER_SIZE, SNAKE_HIDDEN_LAYER_SIZE1, SNAKE_HIDDEN_LAYER_SIZE2, SNAKE_OUTPUT_LAYER_SIZE)
+        self.model1.to(self.device)
+        self.model2.to(self.device)
+        self.last_action = [0] * SNAKE_ACTION_LENGTH
+
+        # Load the weights onto the CPU or GPU
+        if is_load_weights:
+            checkpoint = torch.load(weights_filename, map_location=self.device)
+            self.n_games = checkpoint['epoch'] if is_load_n_games else 0
+            self.model.load_state_dict(checkpoint['model_state_dict'])
+            self.model.eval()
+        else:
+            self.n_games = 0
+
+        self.trainer = DoubleQTrainer(self.model1, self.model2, lr=LR, gamma=self.gamma)
+
+    @property
+    def model(self):
+        return self.model1
+
+    # Update the estimates of action values
+    def train_episode(self, states, actions, rewards):
+        episode_loss = [0]
+        for i in range(1, len(states)):
+            prev_state = states[i - 1]
+            prev_action = actions[i - 1]
+            prev_reward = rewards[i - 1]
+            state = states[i]
+            loss = self.trainer.train_step(prev_state, prev_action, prev_reward, state, done=False)
+            episode_loss.append(loss)
+        
+        # Terminal state
+        loss = self.trainer.train_step(states[-1], actions[-1], rewards[-1], next_state=0, done=True)
+        episode_loss.append(loss)
+        return episode_loss
+
+    def get_action(self, state, is_train=True):
+        """
+        Return vector [0,0,0] with first two positions represent rotation, third pushes forward.
+        Adjusted for epsilon-soft policy.
+        """
+        # Linear decay rate
+        if is_train:
+            self._update_epsilon_linear()
+        else:
+            self.epsilon = SNAKE_MIN_EPSILON
+
+        final_move = [0] * SNAKE_ACTION_LENGTH
+
+        if np.random.rand() < self.epsilon:
+            # Distribute a small probability to all actions, keeping the majority for the best action
+            probabilities = np.ones(SNAKE_ACTION_LENGTH) * (self.epsilon / SNAKE_ACTION_LENGTH)
+
+            state_tensor = torch.tensor(state, dtype=torch.float)
+            with torch.no_grad():  # No need to track gradients here
+                prediction1 = self.model1(state_tensor)
+                prediction2 = self.model2(state_tensor)
+                average_prediction = (prediction1 + prediction2) / 2.0
+                action = torch.argmax(average_prediction).item()
+
+            # Adjust probability for the best action
+            probabilities[action] += (1.0 - self.epsilon)
+
+            # Choose action based on modified probabilities
+            move = np.random.choice(np.arange(SNAKE_ACTION_LENGTH), p=probabilities)
+            final_move[move] = 1
+        else:
+            state_tensor = torch.tensor(state, dtype=torch.float)
+
+            with torch.no_grad():  # No need to track gradients here
+                prediction1 = self.model1(state_tensor)
+                prediction2 = self.model2(state_tensor)
+                average_prediction = (prediction1 + prediction2) / 2.0
+                action = torch.argmax(average_prediction).item()
+
+            final_move[action] = 1
+
+        self.last_action = final_move
+        return final_move
+
+    def _update_epsilon_linear(self):
+        """
+        Update the epsilon value linearly from start_epsilon to end_epsilon over total_games.
+        
+        Parameters:
+        - current_game: The current game number (1 to total_games).
+        - start_epsilon: The starting value of epsilon at game 1.
+        - end_epsilon: The final value of epsilon at game total_games.
+        - total_games: The total number of games over which epsilon will decay.
+        
+        Returns:
+        - Updated epsilon value for the current game.
+        """
+
+        # Calculate the amount of decay per game
+        decay_per_game = (SNAKE_START_EPSILON - SNAKE_MIN_EPSILON) / (self.epochs)
+        
+        # Update epsilon linearly based on the current game number
+        new_epsilon = SNAKE_START_EPSILON - (decay_per_game * (self.n_games))
+        
+        # Ensure epsilon does not go below the end_epsilon
+        self.epsilon = new_epsilon
+        self.epsilon = max(self.epsilon, SNAKE_MIN_EPSILON)
--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import random
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -70,4 +71,57 @@ class QTrainer:
         loss.backward()
 
         self.optimizer.step()
-        return loss
+        return loss.item()
+
+class DoubleQTrainer:
+    def __init__(self, model1, model2, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model1 = model1
+        self.model2 = model2
+        self.optimizer1 = optim.Adam(model1.parameters(), lr=self.lr)
+        self.optimizer2 = optim.Adam(model2.parameters(), lr=self.lr)
+        self.criterion = nn.SmoothL1Loss()
+
+    def train_step(self, state, action, reward, next_state, done):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+        # (n, x)
+
+        if len(state.shape) == 1:
+            # (1, x)
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+
+        # Randomly choose which Q-network to update
+        if random.random() > 0.5:
+            model_to_update = self.model1
+            model_to_estimate = self.model2
+            optimizer = self.optimizer1
+        else:
+            model_to_update = self.model2
+            model_to_estimate = self.model1
+            optimizer = self.optimizer2
+
+        # Q_new = r + y * max(next_predicted Q value) -> only do this if not done
+        if not done:
+            # q_update = reward[0] + self.gamma * torch.max(self.model(next_state).detach())
+            q_update = reward[0] + self.gamma * model_to_estimate(next_state).detach()
+        else:
+            q_update = reward[0]
+
+        q_values = model_to_update(state)
+
+        target = q_values.clone()
+        target[0][torch.argmax(action[0]).item()] = q_update
+    
+        optimizer.zero_grad()
+        loss = self.criterion(target, q_values)
+        loss.backward()
+
+        optimizer.step()
+        return loss.item()
