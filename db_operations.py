6faa48d9ca7340dc13b0a6327dcfa4b3dfbbe552
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-04-07 18:53:38 +0300

    Adde optional era argument.

--- db_operations.py
+++ db_operations.py
@@ -4,8 +4,8 @@ from db_settings import DB_NAME, USER, PASSWORD, HOST, PORT
 from game_settings import LR, SNAKE_GAMMA
 
 class DB_Operations():
-    def __init__(self, total_epochs=0):
-        self._total_epochs = total_epochs
+    def __init__(self):
+        self._last_era = 0
 
     def add_era(self, description):
         self._conn.cursor().execute(
@@ -13,11 +13,25 @@ class DB_Operations():
             (description, LR, SNAKE_GAMMA))
         self._conn.commit()
 
-    def add_epoch(self, score, time, reward, epsilon, bumps, steps, rotations, fk_era_id):
+        self._last_era += 1
+
+    def add_epoch(self, score, time, reward, epsilon, bumps, steps, rotations, era=None):
+        if era is None:
+            era = self._last_era
+
         self._conn.cursor().execute(
             "INSERT INTO epoch (score, time, reward, epsilon, bumps, steps, rotations, fk_era_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", 
-            (score, time, reward, epsilon, bumps, steps, rotations, fk_era_id))
+            (score, time, reward, epsilon, bumps, steps, rotations, era))
         self._conn.commit()
+
+    def _get_last_era(self):
+        cursor = self._conn.cursor()
+
+        # Execute a query to count rows in the table
+        cursor.execute('SELECT COUNT(*) FROM era;')
+
+        # Fetch the result
+        self._last_era = cursor.fetchone()[0]
 
     # For using with 'with' statement
     def __enter__(self):
@@ -29,6 +43,8 @@ class DB_Operations():
             host=HOST, 
             port=PORT,
         )
+
+        self._get_last_era()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
