13fb1300d656ddebdc0eb9abd370fb970a71b252
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-04-07 22:29:12 +0300

    Add csv writer.

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
+import csv
 from agents.double_qlearning import DoubleQLearning
 from agents.qlearning import QLearning
 from agents.n_step_qlearning import NStepQLearning
 from game import SnakeGameAI
 from game_settings import IS_ADD_OBSTACLES, MAPS_FOLDER, SNAKE_WEIGHTS_FILENAME, SCORE_DATA_FILENAME
 from game_settings import GAME_SPEED, SNAKE_SPEED, FOOD_SPEED_MULTIPLIER, FRAME_RESTRICTION
-import time
 from rewards import Rewards
 from game_utils import Timer
 
@@ -29,12 +29,37 @@ class TrainAgent:
         if is_load_weights_snake:
             return
 
-        with open(SCORE_DATA_FILENAME, 'w') as file:
-            file.write('Score, Time, Reward, Epsilon, Bumps, Steps, Rotations\n')
+        # The headers for your CSV file
+        headers = ['Score', 'Time', 'Reward', 'Epsilon', 'Bumps', 'Steps', 'Rotations', 'Average loss']
 
-    def scores_to_csv(self, score, game_duration, snake_reward, snake_epsilon, bumps, steps, rotations):
-        with open(SCORE_DATA_FILENAME, 'a') as file:
-            file.write(f'{str(score)}, {game_duration:.4f}, {snake_reward:.4f}, {snake_epsilon:.4f}, {bumps}, {steps}, {rotations}\n')
+        # Open the CSV file in write mode
+        with open(SCORE_DATA_FILENAME, mode='w', newline='', encoding='utf-8') as file:
+            # Create a CSV writer
+            writer = csv.writer(file)
+
+            # Write the header
+            writer.writerow(headers)
+
+        # with open(SCORE_DATA_FILENAME, 'w') as file:
+            # file.write('Score, Time, Reward, Epsilon, Bumps, Steps, Rotations, Average loss\n')
+
+    def scores_to_csv(self, score, game_duration, snake_reward, snake_epsilon, bumps, steps, rotations, average_loss):
+
+        # Open the CSV file in write mode
+        with open(SCORE_DATA_FILENAME, mode='a', newline='', encoding='utf-8') as file:
+            # Create a CSV writer
+            writer = csv.writer(file)
+            writer.writerow([score, game_duration, snake_reward, snake_epsilon, bumps, steps, rotations, average_loss])
+
+        # with open(SCORE_DATA_FILENAME, 'a') as file:
+        #     file.write(f""" {str(score)}, 
+        #                     {game_duration:.4f}, 
+        #                     {snake_reward:.4f}, 
+        #                     {snake_epsilon:.4f}, 
+        #                     {bumps}, 
+        #                     {steps}, 
+        #                     {rotations},
+        #                     {average_loss}\n""")
 
     def train(self, obstacles_to_load=None):
         self.assure_data_csv()
@@ -44,8 +69,8 @@ class TrainAgent:
 
         for _ in range(games_to_play):
             loss = self._train_single_game()
-            self._losses.append(loss)
             # self.snake_agent.train_episode(self._states, self._actions, self._rewards, self._dones)
+
             self._clear_game_data()
 
     def _train_single_game(self):
@@ -80,6 +105,10 @@ class TrainAgent:
             else:
                 self._rotations += 1
 
+            self._dones.append(float(done))
+            loss = self.snake_agent.train_n_steps(self._states, self._actions, self._rewards, self._dones)
+            self._losses.append(loss)
+
             if done:
                 self._dones.append(1)
                 elapsed_time = timer.get_elapsed_time()
@@ -92,14 +121,16 @@ class TrainAgent:
                     max_reward = self._snake_game_reward
                     self.snake_agent.model.save(epoch=self.snake_agent.n_games, filename=SNAKE_WEIGHTS_FILENAME)
 
-                self.scores_to_csv(self.game.score, elapsed_time, self._snake_game_reward, self.snake_agent.epsilon, self._bumps, self._steps, self._rotations)
+                average_loss = sum(self._losses) / len(self._losses)
+                self.scores_to_csv(self.game.score, 
+                                    elapsed_time, 
+                                    self._snake_game_reward, 
+                                    self.snake_agent.epsilon, 
+                                    self._bumps, 
+                                    self._steps, 
+                                    self._rotations,
+                                    average_loss,)
                 break
-            else:
-                self._dones.append(0)
-            
-            # self.snake_agent.train_step(self._states, self._actions, self._rewards, self._dones)
-            loss = self.snake_agent.train_n_steps(self._states, self._actions, self._rewards, self._dones)
-        return loss
 
     def _clear_game_data(self):
         self._snake_game_reward = 0
@@ -113,6 +144,7 @@ class TrainAgent:
         self._actions.clear()
         self._rewards.clear()
         self._dones.clear()
+        self._losses.clear()
 
 
 is_load_weights_snake = False
