17d1ee2f9ce8ec85fb2282c167c89f21bc6d354d
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-04-09 15:08:41 +0300

    Implemented n-step off-policy qagent and qtrainer.

---
+++ trainers/n_step_off_policy_qtrainer.py
@@ -0,0 +1,92 @@
+from functools import reduce
+import operator
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from game_settings import SNAKE_ACTION_LENGTH, WEIGHT_DECAY
+
+
+class NStepOffPolicyQTrainer:
+    def __init__(self, model, lr, gamma, n_steps=0):
+        self._alpha = lr
+        self._gamma = gamma
+        self._model = model
+        self._optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=WEIGHT_DECAY)
+        self._criterion = nn.SmoothL1Loss()
+        self._n_steps = n_steps
+
+    def train_n_steps(self, states: list, actions: list, rewards: list, dones: int, last_index=0, epsilon=1):
+        if len(states) < self._n_steps:
+            return 0
+
+        if last_index == 0:
+            last_index = len(states)
+
+        states = torch.tensor(states, dtype=torch.float)
+        actions = torch.tensor(actions, dtype=torch.long)
+
+        # Ro
+        ratio = self._importance_sampling_ratio(states, epsilon)
+        # print(self._alpha * ratio)
+        # print()
+
+        for param_group in self._optimizer.param_groups:
+            param_group['lr'] = self._alpha * ratio
+
+        # G
+        rewards_gamma_sum = self._calculate_rewards(rewards, last_index=last_index)
+
+        if not dones[last_index - 1]:
+            # G + y**n * max(Q(S_tau+n, a_tau+n))
+            rewards_gamma_sum += self._gamma**self._n_steps * torch.max(self._model(states[last_index - 1]).detach())
+
+        q_values = self._model(states[last_index - self._n_steps])
+
+        target = q_values.clone()
+        target[torch.argmax(actions[last_index - self._n_steps]).item()] = rewards_gamma_sum
+    
+        self._optimizer.zero_grad()
+        loss = self._criterion(target, q_values)
+        loss.backward()
+
+        self._optimizer.step()
+        return loss.item()
+
+    def train_episode(self):
+        pass
+
+    def _calculate_rewards(self, rewards, last_index=None):
+        rewards_gamma_sum = 0
+        if last_index is None:
+            last_index = len(rewards)
+        start_index = last_index - self._n_steps
+
+        for i in range(start_index, last_index):
+            rewards_gamma_sum += rewards[i] * self._gamma**(i - start_index)
+        return rewards_gamma_sum
+
+    def _importance_sampling_ratio(self, states, epsilon, last_index=None):
+        if last_index is None:
+            last_index = len(states)
+        start_index = last_index - self._n_steps
+        ratios = []
+
+        with torch.no_grad():
+            for i in range(start_index, last_index):
+                logits = self._model(states[i])
+
+                # Apply softmax to convert logits to probabilities
+                probabilities = F.softmax(logits, dim=-1)
+                action_index = torch.argmax(probabilities)
+                pi = probabilities[action_index]
+
+                # B
+                b = epsilon * (1 / SNAKE_ACTION_LENGTH) + (1 - epsilon)
+
+                ratios.append(pi/b)
+
+        # Using reduce with operator.mul
+        ratios_multiplied = reduce(operator.mul, ratios)
+        return ratios_multiplied
