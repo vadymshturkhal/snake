64d2043e98782609f45e69b2d59f0a296761a118
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-04-13 23:17:41 +0300

    Added bonus reward for transitions.

--- rewards.py
+++ rewards.py
@@ -1,10 +1,15 @@
+import numpy as np
+
 from game_settings import REWARD_CRAWLING, REWARD_ROTATION
 from game_settings import REWARD_WIN, REWARD_LOOSE
 
+K = 0.001
 
 class Rewards:
     def __init__(self, game):
         self._game = game
+        self._transitions = {}
+        self._transition_counter = 0
 
     def get_snake_reward(self, state, action):
         # Goal reached
@@ -21,4 +26,15 @@ class Rewards:
         else:
             snake_reward = REWARD_ROTATION
 
-        return snake_reward
+        bonus_reward = self._get_bonus(state, action)
+        self._store_transition(state, action)
+        return snake_reward + bonus_reward
+
+    def _get_bonus(self, state, action):
+        tau = self._transition_counter - self._transitions.get((tuple(state), action), 0)
+        bonus_reward = K * np.sqrt(tau)
+        return bonus_reward
+
+    def _store_transition(self, state, action):
+        self._transitions[(tuple(state), action)] = self._transition_counter
+        self._transition_counter += 1
