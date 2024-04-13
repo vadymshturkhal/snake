6e8dd09ca7f93d41ff9e240155290e2b2caca5ec
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-04-13 22:31:04 +0300

    Changed signature of rewards method.

--- rewards.py
+++ rewards.py
@@ -4,15 +4,15 @@ from game_settings import REWARD_WIN, REWARD_LOOSE
 
 class Rewards:
     def __init__(self, game):
-        self.game = game
+        self._game = game
 
-    def get_snake_reward(self, action):
+    def get_snake_reward(self, state, action):
         # Goal reached
-        if self.game.is_eaten_food:
+        if self._game.is_eaten_food:
             return REWARD_WIN
 
         # Crashed
-        if self.game.snake_is_crashed:
+        if self._game.snake_is_crashed:
             return REWARD_LOOSE
 
         # Penalty
