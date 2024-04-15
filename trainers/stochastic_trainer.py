e1a92172ad0c25ff076daf64b0fde487b3e39087
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-04-15 21:30:50 +0300

    Added stochastic trainer. Doesn't work.

---
+++ trainers/stochastic_trainer.py
@@ -0,0 +1,57 @@
+from functools import reduce
+import operator
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from game_settings import SNAKE_ACTION_LENGTH, WEIGHT_DECAY, MIN_LR, MAX_LR
+
+
+class StochasticTrainer:
+    def __init__(self, model, lr, gamma):
+        self._alpha = lr
+        self._gamma = gamma
+        self._model = model
+        self._optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=WEIGHT_DECAY, momentum=0.9, foreach=False)
+        self._criterion = nn.MSELoss()
+
+    def train_episode(self, states: list, actions: list, rewards: list, dones: int):
+        if len(states) < 2:
+            return 0
+
+        losses = []
+        for i in range(2, len(states)):
+            last_index = i
+            loss = self._train_episode(states, actions, rewards, dones, last_index)
+            losses.append(loss)
+
+        return sum(losses) / len(losses)
+
+    def _train_episode(self, states: list, actions: list, rewards: list, dones: int, last_index=0):
+        states = torch.tensor(np.array(states), dtype=torch.float)
+        actions = torch.tensor(np.array(actions), dtype=torch.long)
+
+        # Perform a forward pass to get the value estimate
+        state_tensor = torch.tensor(states[last_index], dtype=torch.float32)
+
+        # G
+        rewards_gamma_sum = self._calculate_rewards(rewards, last_index=last_index)
+
+        self._optimizer.zero_grad()
+        value_estimate = self._model(state_tensor)
+        value_estimate.backward()
+
+        with torch.no_grad():
+            for param in self._model.parameters():
+                param -= self._alpha * (rewards_gamma_sum - value_estimate) * param.grad
+
+    def _calculate_rewards(self, rewards, last_index=None):
+        rewards_gamma_sum = 0
+        if last_index is None:
+            last_index = len(rewards)
+
+        for i in range(last_index + 1):
+            rewards_gamma_sum += rewards[i] * self._gamma**(i)
+        return rewards_gamma_sum
